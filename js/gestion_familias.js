d4b473a18883625f6057c8063e4fd271cbabcb43
yulianup <yulianup93@gmail.com>
2017-05-25 23:57:45 -0600

    Add files via upload

--- js/gestion_familias.js
+++ js/gestion_familias.js
@@ -1,12 +1,31 @@
 var clasesBoton   =['btn btn-primary glyphicon glyphicon-edit','btn btn-default glyphicon glyphicon-edit'];
 var editores      = ['Familia','Genero','Especie']
 var edicionActual ='';
-//var itemSelect ='xxxx-xxx';    // Lista seleccionable
+var item_select ='';    // Lista seleccionable
 var Sfamilia = "";
 var Sgenero = "";
 var Sespecie = "";
 
-
+function guardarFamilia(nuevo,anterior){
+    document.getElementById("input_nuevo").value="";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var respuesta = this.responseText;            
+            if (respuesta.length>0){
+                //var ddl = document.getElementById("ddGenero");
+                //ddl.innerHTML = respuesta;
+                alert(respuesta);
+            }
+        }
+    }; 
+    nuevo = nuevo.trim();
+    anterior = anterior.trim();
+    var parametros="opcion=setFamilia"+"&familiaNue="+familia+"&familiaAnt="+anterior;
+    xmlhttp.open("POST", "../../scripts/gestion_familias_edit.php?", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(parametros);
+}
 
 function cargarDatos_familiaGeneros(familia){    
     document.getElementById("ddGenero").innerHTML="";
@@ -57,13 +76,16 @@ function despl_ListSeleccionable(indEditor){
     };
     parametros="";
     switch (indEditor) {
-    case "0":
+    case "0":        
+        document.getElementById("bt_edit_selectList").style.display = "inline-block"; 
         parametros = "opcion=getList_Familia";
         break; 
     case "1":
+    document.getElementById("bt_edit_selectList").style.display = "none";
         parametros = "opcion=getList_FamiliaGenero"+"&familia="+familia;
         break;
     case "2":
+    document.getElementById("bt_edit_selectList").style.display = "block";
         parametros = "opcion=getList_FamiliaGeneroEspecie"+"&familia="+familia+"&genero="+genero;
         break;
     default:
@@ -97,7 +119,7 @@ function selectEspecie(especie){
 
 function cerrarAreaEdicion(){
     edicionActual ='';
-    //itemSelect ='';
+    item_select ='';
     document.getElementById("areaEdicion").style.display ="none";
     document.getElementById("area_selectList").style.display ="none";
     document.getElementById("area_listCompleta").style.display ="none";
@@ -123,14 +145,14 @@ function deshabilitar_BnEdicion(valor,clase){
 
 function desp_AreaEdicion(indEditor,idObject){     
     var puede = true;
-    alert(indEditor+" " +Sfamilia+" " +Sgenero+" " + puede);    
+    //alert(indEditor+" " +Sfamilia+" " +Sgenero+" " + puede);    
     if (indEditor == "1")
         puede = (Sfamilia != "");
     else{ 
         if (indEditor == "2")
             puede = (Sfamilia != "") && (Sgenero != "");
     }
-    alert(indEditor+" " +Sfamilia+" " +Sgenero+" " + puede);
+    //alert(indEditor+" " +Sfamilia+" " +Sgenero+" " + puede);
     if (puede) {
         deshabilitar_BnEdicion(true,1);
         edicionActual=editores[indEditor];
@@ -147,7 +169,7 @@ function desp_AreaEdicion(indEditor,idObject){
     }
 }
  
-function desp_SAreaNuevo(){
+/*function desp_SAreaNuevo(){
     document.getElementById("area_nuevo_edit").style.display ="block";
     document.getElementById("lbNuevo_edit").innerText = 'Agregar ' + edicionActual;
     document.getElementById("lblItemSelect").innerText = 'Nuevo';
@@ -158,7 +180,7 @@ function desp_SAreaEditar(){
     document.getElementById("area_nuevo_edit").style.display ="block";
     document.getElementById("lbNuevo_edit").innerText = 'Editar ' + edicionActual;
     document.getElementById("lblItemSelect").innerText = 'Valor actual: ' + itemSelect();
-}
+}*/
 function desp_SAreaEditarEsp(){     
     //document.getElementById("area_nuevo_edit_esp").style.display =block";
     //document.getElementBy-Id("lbNuevo_edit_Esp").innerText = 'Editar ' + editores[0];
@@ -174,7 +196,7 @@ function desp_SAreaListCompleta(){
 
 
 
-function editar_selectList(){
+/*function editar_selectList(){
     document.getElementById("area_listCompleta").style.display ="none";
     if ((edicionActual == editores[0]) || (edicionActual == editores[1]) ){
         desp_SAreaEditar();
@@ -183,14 +205,70 @@ function editar_selectList(){
             alert('Pendiente');
         }
     }
+}*/
+function add_edit_selectList(opcion){ 
+    document.getElementById("area_listCompleta").style.display ="none";  
+    if (opcion == 0) {
+        item_select="";
+        switch (edicionActual) {
+            case editores[0]:
+                desp_area_nuevo_edit(0);
+                break; 
+            case editores[1]:
+                desp_SAreaListCompleta();
+                break;
+            case editores[2]:
+                desp_area_nuevo_edit(0);
+                break;
+        }
+    } else{
+        if (opcion == 1) {
+            switch (edicionActual) {
+                case editores[0]:
+                    desp_area_nuevo_edit(1);
+                    break; 
+                case editores[1]:
+                    break;
+                case editores[2]:
+                    desp_area_nuevo_edit(1);
+                    break;
+            }
+        }
+    }            
 }
-function agregar_selectList(){     
-    if (edicionActual == editores[0]){         
-        desp_SAreaNuevo();
-    }else{
-    desp_SAreaListCompleta();
+function desp_area_nuevo_edit(opcion){
+    area = document.getElementById("area_nuevo_edit");    
+    area.style.display = "block";
+    lbl = document.getElementById("lbNuevo_edit");
+    txtNuevo = document.getElementById("input_nuevo");
+    if (opcion == 0) {        
+        lbl.innerText = "Agregar " + edicionActual;
+        txtNuevo.innerText = "";
+    }
+    else{
+        lbl.innerText = "Editar " + edicionActual;
+        txtNuevo.innerText = item_select;
     }
 }
+function guardarNuevoEdit(){
+    txtNuevo = document.getElementById("input_nuevo");
+    nombre = txtNuevo.value;
+    nombre = nombre.trim();
+    if (nombre!=""){
+        switch (edicionActual) {
+            case editores[0]:
+                guardarFamilia(nombre,item_select);
+                break; 
+            case editores[1]:
+                alert("Pendiente");
+                break;
+            case editores[2]:
+                alert("Pendiente");
+                break;
+        }
+    }
+}
+
 function quitar_selectList(){     
     alert('Pendiente - quietar asociacion y de la lista');
 }
